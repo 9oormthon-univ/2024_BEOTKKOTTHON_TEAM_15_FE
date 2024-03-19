d2e0b023432da034ddfb7635848310024c1b2c71
gominzip <ahaminji@naver.com>
2024-03-19 21:03:58 +0900

    feat : 그룹현황 퍼블리싱(#2)

--- src/components/home/MyGroupStateSection.tsx
+++ src/components/home/MyGroupStateSection.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
 import NoneNoticeCard from '../common/NoneNoticeCard';
+import GroupList from './GroupList';
+
+const sample = [{
+    group : '구름톤구름톤구름톤구름톤구름톤구름톤구름',
+    profile: '/img/nothingLogo.png',
+    count: 22
+},{
+    group : '구름톤 유니브 3기',
+    profile: '/img/nothingLogo.png',
+    count: 22
+},{
+    group : '구름톤 유니브 4기',
+    profile: '/img/nothingLogo.png',
+    count: 22
+},{
+    group : '구름톤 유니브 5기',
+    profile: '/img/nothingLogo.png',
+    count: 22
+},{
+    group : '구름톤 유니브 6기',
+    profile: '/img/nothingLogo.png',
+    count: 22
+},{
+    group : '구름톤 유니브 7기',
+    profile: '/img/nothingLogo.png',
+    count: 22
+},{
+    group : '구름톤 유니브 8기',
+    profile: '/img/nothingLogo.png',
+    count: 22
+},
+]
 
 const MyGroupStateSection = () => {
 	return (
 		<Main>
-			<NoneNoticeCard text="아직 생성된 그룹이 없어요!" />
+			{/* <NoneNoticeCard text="아직 생성된 그룹이 없어요!" /> */}
+			<GroupList dataList={sample}/>
 		</Main>
 	);
 };
