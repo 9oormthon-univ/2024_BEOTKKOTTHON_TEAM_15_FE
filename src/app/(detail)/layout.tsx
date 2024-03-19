5e6fe02b100335ae0de265fc54b7b65fac60e0da
gominzip <ahaminji@naver.com>
2024-03-20 02:35:52 +0900

    feat : 남은 페이지 라우팅 처리 및 추가 헤더 구현

---
+++ src/app/(detail)/layout.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import StyledComponentsRegistry from '../lib/registry';
+import styled from 'styled-components';
+import NavBar from '@/components/common/NavBar';
+import Header from '@/components/common/Header';
+import DetailHeader from '@/components/common/DetailHeader';
+
+export default function layout({ children }: { children: React.ReactNode }) {
+	return (
+		<StyledComponentsRegistry>
+			<Main>
+				<MainBox>
+					<DetailHeader/>
+					{children}
+				</MainBox>
+				<NavBar />
+			</Main>
+		</StyledComponentsRegistry>
+	);
+}
+
+const Main = styled.div`
+	width: 100%;
+	min-height: 95vh;
+	height: fit-content;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	background-color: #93613B;
+`;
+
+const MainBox = styled.div`
+	background-color: #fff9ee;
+	width: 98%;
+	min-height: 95vh;
+	height: fit-content;
+	font-family: Pretendard;
+  padding-bottom: 6rem;
+  background-color: #FFF5E0;
+	@media (min-width: 0px) {
+		html {
+			font-size: 8px;
+		}
+	}
+
+	@media (min-width: 600px) {
+		html {
+			font-size: 10px;
+		}
+		width: 600px;
+	}
+
+	@media (min-width: 1024px) {
+		html {
+			font-size: 10px;
+		}
+		width: 600px;
+	}
+`;
