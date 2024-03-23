5ea5286b55238d66935a69c2a02d9fcf81c79925
CJY <cjy3458@naver.com>
2024-03-24 06:42:21 +0900

    feat: 주석 및 콘솔 제거

--- src/components/onBoarding/LeaderOnBoarding/Leader.tsx
+++ src/components/onBoarding/LeaderOnBoarding/Leader.tsx
@@ -28,7 +28,6 @@ const Leader = () => {
 
 		// 객체를 JSON 문자열로 변환하여 추가
 		const dto = JSON.stringify({ name: groupName, description: description });
-		console.log(dto);
 		const blob = new Blob([dto], { type: 'application/json' });
 		formData.append('teamSaveRequestDto', blob);
 		// formData.append('name', groupName);
@@ -43,7 +42,6 @@ const Leader = () => {
 
 	const handleSubmit = async (e: React.FormEvent) => {
 		e.preventDefault();
-		console.log(groupImage);
 		if (!groupName) {
 			alert('그룹 이름을 적어주세요!');
 			return;
