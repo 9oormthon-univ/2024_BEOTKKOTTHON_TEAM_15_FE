19b7768750b263ea1d01fdbf2164de12f70f3eb4
CJY <cjy3458@naver.com>
2024-03-24 00:22:30 +0900

    feat: 알림 페이지 구현

--- src/app/(detail)/[tab]/alert/page.tsx
+++ src/app/(detail)/[tab]/alert/page.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import Alert from '@/components/alert/Alert';
+import React from 'react';
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+	return <Alert />;
+};
 
-export default page
+export default page;
